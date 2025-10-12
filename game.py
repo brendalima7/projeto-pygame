206305527a7ff7a4eb5d01800814fe6c2622456c
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2025-10-12 17:22:02 -0300

    estrutura base com set_mode fullscreen

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
-# commit teste
+import pygame
+
+pygame.init()
+
+info = pygame.display.Info()
+WINDOWWIDHT = info.current_w
+WINDOWHEIGHT = info.current_h
+
+window = pygame.display.set_mode((WINDOWWIDHT, WINDOWHEIGHT))
+pygame.display.set_caption('Game')
+
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+        keys = pygame.key.get_pressed()
+        if event.type == pygame.KEYDOWN:
+            if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
+                game = False
+
+    window.fill((0, 0, 0))
+    pygame.display.update()
